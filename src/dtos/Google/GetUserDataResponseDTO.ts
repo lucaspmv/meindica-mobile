f1ad8c910e11ee346f9ea47a75929e9ff9d74471
Lucas Porto <lucaspmv@hotmail.com>
2023-03-30 22:35:38 -0300

    merge add-google-auth into dev

---
+++ src/dtos/Google/GetUserDataResponseDTO.ts
@@ -0,0 +1,13 @@
+interface GetUserDataResponseDTO {
+  email: string;
+  family_name: string;
+  given_name: string;
+  hd: string;
+  id: string;
+  locale: string;
+  name: string;
+  picture: string;
+  verified_email: boolean;
+}
+
+export { GetUserDataResponseDTO };
